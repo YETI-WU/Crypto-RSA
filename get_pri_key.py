12bced25da8114dc70eb8b91de7e4ff0a095d786
YETI-WU <34374815+YETI-WU@users.noreply.github.com>
2018-12-04 09:46:22 -0600

    Update get_pri_key.py

--- get_pri_key.py
+++ get_pri_key.py
@@ -17,10 +17,10 @@ def get_factors(n):
     while not math.sqrt(b2).is_integer():
         a += 1
         b2 = a**2 - n
-    #print('result : a = {}, b2 = {}'.format(a,b2))
+    print('result : a = {}, b2 = {}'.format(a,b2))
     p = int( a + math.sqrt(b2) )
     q = int( a - math.sqrt(b2) )
-    #print('n = {} : p = {}, q = {}'.format(n,p,q))
+    print('n = {} : p = {}, q = {}'.format(n,p,q))
     return p, q
 
 
@@ -30,8 +30,7 @@ def get_key(p, q, e):
     # Extended Euclidean Algorithms (two value are coprime)
     totient = (p-1) * (q-1)
     m = totient
-    y = 0
-    x = 1
+    y , x = 0 , 1
     while e > 1:
         q = e // m  # quotient
         t = m
@@ -51,4 +50,30 @@ def get_key(p, q, e):
     return d
 
 
-(to be continue.....................................)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        usage()
+        
+    n , e , all_key = 0 , 0 , None
+    with open("keys4student.jason", "r") as f:
+        all_keys = json.load(f)
+    
+    name = hashlib.sha224(sys.argv[1].encode('utf-8').strip()).hexdigest()
+    if name not in all_keys:
+        print(sys.argv[1] + ' not in keylist')
+        usage()
+        
+    pub_key = all_keys[name]
+    n = int(pub_key['N'], 16)
+    e = int(pub_key['e'], 16)
+    print('Your public key: (' + hex(n).rstrip('L') + ', ' + hex(e).rstrip('L') + ')')
+    
+    # find p & q value that n = p * q
+    (p,q) = get_factors(n)
+    
+    # Use p, q, e, to find the value d (Burte Force will take a long long time)
+    d = get_key(p,q,e)
+    
+    print('Your private eky: ' + hex(d).rstrip('L') )
+    
+
